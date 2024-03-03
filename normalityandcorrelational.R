737e43df83ff6c03fce775e2faef690e0a6a339d
Savindu <savinduruhunuhewa@gmail.com>
2024-03-03 19:21:44 +0530

    Detach the dataset and add new dataset

--- normalityandcorrelational.R
+++ normalityandcorrelational.R
@@ -7,13 +7,23 @@ View(fertilizer_data)
 ### check the data type
 str(fertilizer_data)
 
-### biding the dataset with  environment
+### binding the dataset with  environment
 attach(fertilizer_data)
+### view the dataser
+View(fertilizer_data)
 ###check the null values
 fertilizer_data[!complete.cases(fertilizer_data),]
 
 ### Preparing the data sets( we calles as data preprocessing)
 install.packages('plyr')
 library('plyr')
-fertilizer_datas$Crop_Yield <- revalue(Crop_Yield, c(Wheat=7,Rice=6,Corn=5,Soybean=4, Barley=3, Oats=2,Potato=1))
+fertilizer_data$Crop_Type <- revalue(Crop_Type, c( Wheat=7,Rice=6,Corn=5,Soybean=4, Barley=3, Oats=2,Potato=1))
 write.csv(fertilizer_data,"fertilizer_data_New.csv")
+
+### detach the fertilizer_data dataset
+detach(fertilizer_data)
+### import the fertilizer_data_New dataset
+fertilizer_data_New <- read.csv("fertilizer_data_New.csv")
+fertilizer_data_New
+### attach the fertilizer_data_New dataset
+attach(fertilizer_data_New)
